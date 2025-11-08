54dbe8f2b7b5bea97f85ed2f7e8af56b4e73d82f
vidhit10 <vidhitchandra@gmail.com>
2025-11-08 18:47:12 -0500

    Modify emotion file to include formatting requirement

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,4 @@
-import requests
+import requests, json
 
 def emotion_detector(text_to_analyze):
 
@@ -6,4 +6,15 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     payload = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json=payload, headers=header)
-    return response.text
+    formatted_response = json.loads(response.text)
+    print(formatted_response)
+    anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    emotions = [anger, disgust, fear, joy, sadness]
+    dominant_emotion_score = max(emotions)
+    dominant_emotion=[k for k,v in formatted_response['emotionPredictions'][0]['emotion'].items() if v == dominant_emotion_score][0] 
+    formatted_response_dict = {'anger': anger, 'disgust': disgust, 'fear': fear, 'joy': joy, 'sadness': sadness, 'dominant_emotion': dominant_emotion}
+    return formatted_response_dict
